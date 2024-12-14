8772857bc34f220f1c1322d2396a96ea394f4cf5
shubhambaidhya <shubhambaidhya7@gmail.com>
2024-12-14 11:46:48 +0545

    Button_intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import CustomCard from "./components/card/Card";
+import CustomButton from "./components/button/CustomButton";
 function App() {
   return (
     <div>
-      <CustomCard />
+      <CustomButton />
     </div>
   );
 }
